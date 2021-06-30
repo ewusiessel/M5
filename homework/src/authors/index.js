8c9898910eea20aa512187e07b728053ef61f807
Ekow <ewusiessel@gmail.com>
2021-06-30 22:47:54 +0100

    implemented GET functionality

--- homework/src/authors/index.js
+++ homework/src/authors/index.js
@@ -1,34 +1,51 @@
 import express from "express";
 import fs from "fs";
+import uniqid from "uniqid";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-
-
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = dirname(__filename);
 
-const authorsFilepath = path.join(__dirname, "authors.json");
+const authorsFilePath = path.join(__dirname, "authors.json");
 
 const router = express.Router();
-
 
 //get all authors
 router.get("/", async (req, res, next) => {
   try {
     const fileAsBuffer = fs.readFileSync(authorsFilePath);
-    const fileAsString = fileAsBuffer.toString(); 
+    const fileAsString = fileAsBuffer.toString();
     const fileAsJSON = JSON.parse(fileAsString);
     res.send(fileAsJSON);
   } catch (error) {
-    res.send(500).send({ message: error.message });
+    res.sendStatus(500).send({ message: error.message });
   }
 });
 
-//create authors
+//create author
 router.post("/", async (req, res, next) => {
   try {
+    const { name, surname, email, dateOfBirth } = req.body;
+
+    const author = {
+      id: uniqid(),
+      name,
+      surname,
+      email,
+      dateOfBirth,
+      avatar: `https://ui-avatars.com/?name=${name}+${surname}`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const fileAsBuffer = fs.readFileSync(authorsFilePath);
+    const fileAsString = fileAsBuffer.toString();
+    const fileAsJSONArray = JSON.parse(fileAsString);
+
+    fileAsJSONArray.push(author);
+    fs.writeFileSync(authorsFilePath, JSON.stringify(fileAsJSONArray));
+    res.send(author);
   } catch (error) {
     res.send(500).send({ message: error.message });
   }
