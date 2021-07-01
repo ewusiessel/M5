6a294e952cd135902b8e1b5bf877d74b34e5e377
Ekow <ewusiessel@gmail.com>
2021-07-01 22:12:41 +0100

    access to public folder granted

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./authors/index.js";
 import blogsRouter from "./blogs/index.js";
 import { notFound, forbidden, catchAllErrorHandler } from "./errorHandlers.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const publicDirectory = path.join(__dirname, "../public");
 
 const server = express();
+
 const PORT = 3001;
 
 server.use(cors());
