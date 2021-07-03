ec1fc312fe8581e6a5883f9f728227dd64d593f5
Ekow <ewusiessel@gmail.com>
2021-07-03 22:44:53 +0100

    add router file and requests

---
+++ strive-box-api/src/services/files/router.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+
+const router = Router();
+
+//1. GET
+
+router.get("/", () => {
+  try {
+  } catch (error) {}
+});
+
+//2. POST
+
+router.post("/", () => {
+  try {
+  } catch (error) {}
+});
+
+//3. GET SINGLE
+
+router.get("/:id", () => {
+  try {
+  } catch (error) {}
+});
+
+//4. UPDATE
+
+router.put("/:id", () => {
+  try {
+  } catch (error) {}
+});
+
+//5. DELETE SINGLE
+
+router.get("/:id", () => {
+  try {
+  } catch (error) {}
+});
+
+export default router;
